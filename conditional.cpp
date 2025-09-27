9d6e9f9b1564dfb1f44362afa8d65804dd74a037
prajeevranjan <prajeevranjan1680@gmail.com>
2025-09-27 19:49:46 +0530

    conditional done

---
+++ conditional.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+
+    // if statement
+    if (a = 5){
+        cout << "a is equal to 5" << endl;
+    }
+
+    // else-if statement
+    else if ( a < 5){
+        cout << "a is less than 5" << endl;
+    }
+
+    // else statement 
+    else{
+        cout << " a is greater than 5" << endl; 
+    }
+}
---
+++ io.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout << "enter a numebr : ";     // prompt
+    cin >> a;     // input operator
+    cout << 5*a << endl;    // output operator
+    return 0;
+}
