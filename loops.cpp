c1eaaf440b05ad80af477ba961fbafb0da39921f
prajeevranjan <prajeevranjan1680@gmail.com>
2025-09-28 18:46:03 +0530

    loops done!

---
+++ loops.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    // for loop
+    for(int i=0; i < 5; i++){
+        cout << "hello world" << endl;
+    }
+
+    // while loop
+    int k = 0;
+    while(k < 5){
+        cout << "hello" << endl;
+        k++;
+    }
+
+    // do-while loop
+    int j = 0;
+    do{
+        cout << "rajeev" << endl;
+        j++;
+    }while(j < 5);
+}
