1f85a6542c43788e3a81c9345bf2223b18f1f0c2
prajeevranjan <prajeevranjan1680@gmail.com>
2025-09-28 19:05:25 +0530

    more on array done!

--- array.cpp
+++ array.cpp
@@ -3,8 +3,24 @@ using namespace std;
 
 int main(){
     int arr[] = {1, 2, 3, 4, 5};
+
+    // accessing array elements
+    cout << arr[3] << endl;
+
+    // array update
+    cout << arr[1] << endl;
+    arr[1] = 22;
+    cout << arr[1] << endl;
+
+    // size of array
+    int n = sizeof(arr);
+    cout << n << endl;
+
+    // Traversing and printing arr
     for ( int i = 0; i<5; i++){
         cout << arr[i] << endl;
     }
+
     return 0;
+
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+// Traversing and printing arr
+    // for ( int i = 0; i<5; i++){
+    //     cout << arr[i] << endl;
+    // }
+
+    // // accessing array elements
+    // cout << arr[3] << endl;
+
+    // // array update
+    // arr[1] = 22;
+    // cout << arr[1] << endl;
