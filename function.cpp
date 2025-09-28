a9803aea9bb4a7cadbdeca0dd0a1799dcf8fe78d
prajeevranjan <prajeevranjan1680@gmail.com>
2025-09-28 18:48:24 +0530

    function done

---
+++ function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Void function definition
+void hello() {
+    cout << "Rajeev Ranjan Pandey" << endl;
+}
+
+// Return-type function definition
+int square(int x) {
+    return x * x;
+}
+
+int main() {
+
+    // Calling the void function
+    hello();
+
+    // Calling the return-type function
+    int result = square(5);
+    cout << "Square of 5 is: " << result << endl;
+
+    return 0;
+}
